fceba4873d0fb45dbefe39b2a5e22859128addd2
test_matlab <51116957+fanhantianxia@users.noreply.github.com>
2019-09-22 19:25:04 +0800

    Add files via upload

---
+++ wb_pipeline_calcFOCA.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+input_dir=$1  # /data
+output_dir=$2
+combs_project_id=$3
+
+FOCA_TR = $4     #2
+
+echo FOCA_TR 
+
+#--matlab config--
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/MATLAB/MATLAB_Runtime/v90/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/server:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64
+export XAPPLRESDIR=/usr/local/MATLAB/MATLAB_Runtime/v90/X11/app-defaults
+export MCR_CACHE_VERBOSE=true
+export MCR_CACHE_ROOT=/tmp
+
+/root/matlab_script/wb_pipeline_FOCA $input_dir $output_dir /file_buf/brain_mask.nii FOCA_TR #[TR] 
+
+echo 'END'
