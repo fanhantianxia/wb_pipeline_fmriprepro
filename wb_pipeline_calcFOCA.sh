b18db833de189f2e5fdfa1e85d4235fd1ce59615
test_matlab <51116957+fanhantianxia@users.noreply.github.com>
2019-09-23 21:40:26 +0800

    Update wb_pipeline_calcFOCA.sh

--- wb_pipeline_calcFOCA.sh
+++ wb_pipeline_calcFOCA.sh
@@ -1,11 +1,20 @@
-﻿#!/bin/bash
+#!/bin/bash
 input_dir=$1  # /data
 output_dir=$2
 combs_project_id=$3
 
-FOCA_TR = $4     #2
+FOCA_TR = $4  #default:2
 
-echo FOCA_TR 
+echo '************** wb_pipeline_calcFOCA START *******************'
+sleep 1s
+
+echo '-----------Input_Config_Start--------------'
+echo 'input_dir='$input_dir
+echo 'output_dir='$output_dir
+echo 'combs_project_id='$combs_project_id
+echo 'FOCA_TR='$FOCA_TR
+echo '------------Input_Config_End---------------'
+sleep 3s
 
 #--matlab config--
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/MATLAB/MATLAB_Runtime/v90/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/server:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64
@@ -15,4 +24,4 @@ export MCR_CACHE_ROOT=/tmp
 
 /root/matlab_script/wb_pipeline_FOCA $input_dir $output_dir /file_buf/brain_mask.nii FOCA_TR #[TR] 
 
-echo 'END'
+echo '************** wb_pipeline_calcFOCA END *******************'
