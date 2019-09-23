a564c7838c4353595dfa10ced1d5f507897a06af
test_matlab <51116957+fanhantianxia@users.noreply.github.com>
2019-09-23 20:16:53 +0800

    Update wb_pipeline_calcFCD.sh

--- wb_pipeline_calcFCD.sh
+++ wb_pipeline_calcFCD.sh
@@ -8,9 +8,15 @@ FCD_Thresold=$4    #0.6
 FCD_TR=$5          #2
 FCD_ConnectType=$6 #0
 
-echo $FCD_Thresold
-echo $FCD_TR
-echo $FCD_ConnectType
+echo '-----------Input_Config_Start--------------'
+echo 'input_dir='$input_dir
+echo 'output_dir='$output_dir
+echo 'combs_project_id='$combs_project_id
+echo 'FCD_Thresold='$FCD_Thresold
+echo 'FCD_TR='$FCD_TR
+echo 'FCD_ConnectType='$FCD_ConnectType
+echo '------------Input_Config_END---------------'
+sleep 3s
 
 #--matlab config--
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/MATLAB/MATLAB_Runtime/v90/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/server:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64
@@ -19,7 +25,7 @@ export MCR_CACHE_VERBOSE=true
 export MCR_CACHE_ROOT=/tmp
 
 #mkdir output_dir/FCD  #FCD输出文件夹
-/root/matlab_script/wb_pipeline_FCD $input_dir $output_dir /file_buf/brain_mask.nii $FCD_Thresold $FCD_TR FCD_ConnectType 
+/root/matlab_script/wb_pipeline_FCD $input_dir $output_dir /file_buf/brain_mask.nii $FCD_Thresold $FCD_TR $FCD_ConnectType 
 #[Thresold] [TR] [ConnectType]
 
 echo 'END'
