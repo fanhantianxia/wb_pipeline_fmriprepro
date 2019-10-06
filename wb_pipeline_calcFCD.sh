987f2d35b51f53760f07de0c7bfa5d9527ab17c1
test_matlab <51116957+fanhantianxia@users.noreply.github.com>
2019-10-06 10:16:29 +0800

    Update wb_pipeline_calcFCD.sh

--- wb_pipeline_calcFCD.sh
+++ wb_pipeline_calcFCD.sh
@@ -1,55 +1,177 @@
 #!/bin/bash
-
-error_type=0 
-
 input_dir=$1  # /data
 output_dir=$2
 combs_project_id=$3
-FCD_Thresold=$4    #0.6
-FCD_TR=$5          #2
-FCD_ConnectType=$6 #0
+FCD_BrainMask_option=$4
+FCD_Thresold=$5    #0.6
+FCD_TR=$6          #2
+FCD_ConnectType=$7 #0
 
-echo '************** wb_pipeline_calcFCD START *******************'
-sleep 1s
 
-echo 'input_dir='$input_dir
-echo 'output_dir='$output_dir
-echo 'combs_project_id='$combs_project_id
+#workdir=/home/yufan/Desktop/FCD/data_buf  #debug route
+workdir=/root/FCD/data_buf  #
 
-if $FCD_Thresold>=0 
-  then
-  echo 'FCD_Thresold='$FCD_Thresold
-elif $FCD_Thresold<=1   
-  then
-  echo 'FCD_Thresold='$FCD_Thresold
+
+echo '---------------------------------------------'
+time=$(date "+%Y-%m-%d %H:%M:%S")
+echo "${time}"
+
+#check all inputs
+echo 'Check all inputs......'
+
+#######--Check BrainMask--#######
+echo $FCD_BrainMask_option
+if test $FCD_BrainMask_option = "BrainMask_65X77X45" 
+   then 
+   FCD_BrainMask_dir=/file_buf/BrainMask_65X77X45.nii
+elif test $FCD_BrainMask_option = "BrainMask_53X63X46" 
+   then 
+   FCD_BrainMask_dir=/file_buf/BrainMask_53X63X46.nii
+elif test $FCD_BrainMask_option = "BrainMask_61X73X41" 
+   then 
+   FCD_BrainMask_dir=/file_buf/BrainMask_61X73X41.nii
+elif [ -f $FCD_BrainMask_option ]
+   then 
+   FCD_BrainMask_dir=$FCD_BrainMask_option
 else
-  echo '********FAILED********'
-  error_type=1 
+   echo 'The specified template could not be found'
+fi
+#echo '[DEBUG]FCD_BrainMask_dir='$FCD_BrainMask_dir #debug
 
+
+#######--Check Thresold--#######
+if test -z "$FCD_Thresold" ;then  #is empty
+   FCD_Thresold=0.6
+   echo 'Using default FCD Thresold.'
+elif test $FCD_Thresold = "[]" ;then  #is equal
+   FCD_Thresold=0.6
+   echo 'Using default FCD Thresold.'
+else # >1 || <0 
+   if [ `echo ${FCD_Thresold} | awk -v tem="1.0" '{print($1>=tem)? "1":"0"}'` -eq "1" ] # >=1 
+   then 
+      echo 'Input FCD Thresold is invalid.'
+      FCD_Thresold=0.6
+      echo 'Using default FCD Thresold.'
+   elif [ `echo ${FCD_Thresold} | awk -v tem="0.0" '{print($1<=tem)? "1":"0"}'` -eq "1" ] # <=0
+   then
+      echo 'Input FCD Thresold is invalid.'
+      FCD_Thresold=0.6
+      echo 'Using default FCD Thresold.'
+   fi
+fi
+echo 'FCD_Thresold='$FCD_Thresold
+
+
+#######--Check TR--#######
+if test -z "$FCD_TR" ;then  #is empty
+   FCD_TR=2
+   echo 'Using default FCD TR.'
+elif test $FCD_TR = "[]" ;then  #is equal
+   FCD_TR=2
+   echo 'Using default FCD TR.'
+else
+   if [ `echo ${FCD_TR} | awk -v tem="0.0" '{print($1<=tem)? "1":"0"}'` -eq "1" ] # <=0
+   then
+      echo 'Input FCD TR is invalid.'
+      FCD_TR=2
+      echo 'Using default FCD TR.'
+   fi
+fi
 echo 'FCD_TR='$FCD_TR
 
-if $FCD_ConnectType=0
-  then
-  echo '静态'
-elif $FCD_ConnectType=1
-  then
-  echo '动态'
-else
-  echo '********FAILED********'
-  error_type=1 
+
+#######--Check ConnectType--#######
+if test -z "$FCD_ConnectType" ;then  #is empty
+   FCD_ConnectType=0
+   echo 'Using default FCD ConnectType.'
+elif test $FCD_TR = "[]" ;then  #is equal
+   FCD_ConnectType=0
+   echo 'Using default FCD ConnectType.'
 fi
 
-sleep 3s
+if [ "$FCD_ConnectType" -eq 0 ];then
+   echo 'ConnectType=static'
+elif [ "$FCD_ConnectType" -eq 1 ];then
+   echo 'ConnectType=dynamic'
+else
+   echo 'Input ConnectType is invalid.'
+   FCD_ConnectType=0
+   echo 'Using default FCD ConnectType.'
+   echo 'ConnectType=dynamic'
+fi
 
-#--matlab config--
+
+#display parameters
+echo 'Start Project......'
+echo 'Project ID:'$combs_project_id
+
+#config environment var
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/MATLAB/MATLAB_Runtime/v90/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/server:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64
 export XAPPLRESDIR=/usr/local/MATLAB/MATLAB_Runtime/v90/X11/app-defaults
 export MCR_CACHE_VERBOSE=true
 export MCR_CACHE_ROOT=/tmp
 
-#mkdir output_dir/FCD  #FCD输出文件夹
-/root/matlab_script/wb_pipeline_FCD $input_dir $output_dir /file_buf/brain_mask.nii $FCD_Thresold $FCD_TR $FCD_ConnectType 
-#[Thresold] [TR] [ConnectType]
 
-echo '********SUCCESS********'
-echo '********FAILED********'
+#loading data and calculating
+echo 'loading data and calculating......'
+string=$input_dir
+array=(${string//,/ })
+
+cd $workdir  
+#first unzip
+for SingleZip in ${array[@]}
+do
+   #echo '[DEBUG]'$SingleZip #debug
+   unzip $SingleZip -d $workdir #input_dir connect to workdir
+done
+  
+
+ZipNum=0
+Skipped_FileNum=0
+dir=$(ls -l $workdir  |awk '/^d/ {print $NF}') #give all files name in workdir
+for InputFile in $dir
+do
+   let ZipNum++
+   echo 'No. of subjects:'$ZipNum
+
+   if [ `ls ./$InputFile | wc -c` -eq 0 ] #check if file exits
+   then 
+     echo "file is null"
+     echo "Fail to load file:"$InputFile
+     arr[Skipped_FileNum]=$InputFile
+     let Skipped_FileNum++
+   else
+     cd ./$InputFile
+     #second unzip
+     find . -type f -name "*.zip" | xargs -n1 unzip 
+     rm *.zip
+     cd .. #back to workdir
+
+     #echo 'caluFCD'
+     mkdir $output_dir/FCD/$InputFile #position of result 
+     /root/matlab_script/wb_pipeline_FCD $workdir/$InputFile $output_dir/FCD/$InputFile $FCD_BrainMask_dir $FCD_Thresold $FCD_TR $FCD_ConnectType   
+   fi 
+done
+
+
+echo '------------------------'
+if test -z "$Skipped_FileNum"
+then
+   echo '********SUCCESS********'
+elif test $Skipped_FileNum = $ZipNum
+then
+   echo '********FAILED********'
+else
+   echo '********CalculatedSubjects********' 
+   let CalculatedNum=$ZipNum-$Skipped_FileNum
+   echo 'No. of Calculated Subjects:'$CalculatedNum
+   echo '********SkippedSubjects********'
+   echo 'No. of Skipped Subjects:'$Skipped_FileNum
+
+   for value in ${arr[@]}
+   do
+     echo 'Skipped_filename:'$value
+   done
+
+   echo '********SUCCESS********'
+fi
