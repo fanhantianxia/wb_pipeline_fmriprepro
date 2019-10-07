eeddb754d575210edaa48c08c1ce1b25dc6ced67
test_matlab <51116957+fanhantianxia@users.noreply.github.com>
2019-10-07 20:17:38 +0800

    Update wb_pipeline_calcALL.sh

--- wb_pipeline_calcALL.sh
+++ wb_pipeline_calcALL.sh
@@ -8,6 +8,12 @@ FCD_Thresold=$5    #0.6
 FCDandFOCA_TR=$6              #2
 FCD_ConnectType=$7 #0
 
+preprocessing_options=$8
+Surface_preprocessing_options=$9
+
+mkdir -p /root/FMRIPREP/data_buf
+workdir=/root/FMRIPREP/data_buf 
+
 echo '---------------------------------------------'
 time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "${time}"
@@ -21,7 +27,7 @@ if test $FCDandFOCA_BrainMask_option = "BrainMask_65x77x45"
    BrainMask_dir=/file_buf/BrainMaskFile/BrainMask_65x77x45.nii
 elif test $FCDandFOCA_BrainMask_option = "BrainMask_53x63x46" 
    then 
-   FBrainMask_dir=/file_buf/BrainMaskFile/BrainMask_53x63x46.img
+   BrainMask_dir=/file_buf/BrainMaskFile/BrainMask_53x63x46.img
 elif test $FCDandFOCA_BrainMask_option = "BrainMask_61x73x61" 
    then 
    BrainMask_dir=/file_buf/BrainMaskFile/BrainMask_61x73x41.img
@@ -96,26 +102,80 @@ else
    echo 'ConnectType=dynamic'
 fi
 
+if test -z "$preprocessing_options" 
+   then
+   preprocessing_options_content=
+elif test $preprocessing_options = "[]" 
+   then 
+   preprocessing_options_content=
+elif test $preprocessing_options = "OnlyAnat" 
+   then 
+   preprocessing_options_content='--anat-only'
+   echo 'OK'
+elif test $preprocessing_options="All" 
+   then 
+   preprocessing_options_content=
+else
+   echo '[ERROR] Invalid format or contents of preprocessing_options, please try again!'
+   echo '********FAILED********'
+   exit #exit whole process
+fi
+
+echo 'preprocessing_options_content='$preprocessing_options_content
+
+
+
+
+
+if test -z "$Surface_preprocessing_options" 
+   then
+   Surface_preprocessing_options_content='--no-submm-recon --fs-no-reconall'
+elif test $Surface_preprocessing_options = "[]" 
+   then 
+   Surface_preprocessing_options_content='--no-submm-recon --fs-no-reconall'
+elif test $Surface_preprocessing_options = "Disable" 
+   then 
+   Surface_preprocessing_options_content='--no-submm-recon --fs-no-reconall'
+elif test $Surface_preprocessing_options = "FreeSurfer" 
+   then 
+   Surface_preprocessing_options_content='--no-submm-recon'
+   echo 'OK'
+elif test $Surface_preprocessing_options = "SubmmRecon" 
+   then 
+   Surface_preprocessing_options_content='--fs-no-reconall'
+elif test $Surface_preprocessing_options = "All" 
+   then 
+   Surface_preprocessing_options_content=
+else
+   echo '[ERROR] Invalid format or contents of Surface_preprocessing_options, please try again!'
+   echo '********FAILED********'
+   exit #exit whole process
+fi
+echo 'Surface_preprocessing_options_content='$Surface_preprocessing_options_content
+
+
+
+
 #display parameters
 echo 'Start Project......'
 echo 'Project ID:'$combs_project_id
 
 
-echo '------Input_Config START----------'
-echo 'input_dir='$input_dir
-echo 'output_dir='$output_dir
-echo 'combs_project_id='$combs_project_id
-echo '-------Input_Config END-----------'
+#loading data and calculating
+echo 'loading data and calculating......'
+string=$input_dir
+array=(${string//,/ })
+cd $workdir  
+#first unzip
+for SingleZip in ${array[@]}
+do
+   #echo '[DEBUG] '$SingleZip #debug
+   unzip $SingleZip -d $workdir #input_dir connect to workdir
+done
 
 
-echo 'inspect fmriprep version....'
 fmriprep --version
-
-
 mkdir /DataBuf 
-
-
-fmriprep input_dir output_dir participant -w /DataBuf --no-submm-recon --fs-no-reconall
 
 #--matlab config--
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/MATLAB/MATLAB_Runtime/v90/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/server:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64
@@ -123,13 +183,38 @@ export XAPPLRESDIR=/usr/local/MATLAB/MATLAB_Runtime/v90/X11/app-defaults
 export MCR_CACHE_VERBOSE=true
 export MCR_CACHE_ROOT=/tmp
 
+ZipNum=0
+Skipped_FileNum=0
+dir=$(ls -l $workdir  |awk '/^d/ {print $NF}') #give all files name in workdir
+for InputFile in $dir
+do
+   let ZipNum++
+   echo '------------------------'
+   echo 'No. of subjects:'$ZipNum
+   
+   if [ `ls ./$InputFile | wc -c` -eq 0 ] #check if file exits
+   then 
+     echo "[Warning] file is null"
+     echo "[Warning] Fail to load file:"$InputFile
+     arr[Skipped_FileNum]=$InputFile
+     let Skipped_FileNum++
+   else
+     mkdir -p $output_dir/FMRIPREP/$InputFile
+     fmriprep $workdir/$InputFile $output_dir/FMRIPREP/$InputFile participant -w /DataBuf --no-submm-recon --fs-no-reconall
+
+
+   fi 
+done
+
+
+
 
 mkdir -p /file_buf
 cd /file_buf
 git clone https://github.com/fanhantianxia/wb_pipeline_fmriprepro.git /file_buf
 
 mkdir /nit  
-find /out/fmriprep -type f -name "*desc-preproc_bold.nii.gz" | xargs cp -t  /nit
+find $output_dir/FMRIPREP/$InputFile -type f -name "*desc-preproc_bold.nii.gz" | xargs cp -t  /nit
 gzip  -d  /nit/*_bold.nii.gz
 
 #在/nit里添加config.json和fmriprep2nit.py
@@ -139,9 +224,9 @@ cd /nit
 python fmriprep2nit.py  #需要修改一下程序内部路径
 
 mkdir -p $output_dir/FCD  #FCD输出文件夹
-/root/matlab_script/wb_pipeline_FCD /nit/nit_Input $output_dir/FCD /file_buf/brain_mask.nii 0.6 2 0 #[Thresold] [TR] [ConnectType]
+/root/matlab_script/wb_pipeline_FCD /nit/nit_Input $output_dir/FCD/$InputFile $BrainMask_dir $FCD_Thresold $FCDandFOCA_TR $FCD_ConnectType 
 
 mkdir -p $output_dir/FOCA  #FOCA输出文件夹
-/root/matlab_script/wb_pipeline_FOCA /nit/nit_Input $output_dir/FOCA /file_buf/brain_mask.nii 2
-echo 'END'
+/root/matlab_script/wb_pipeline_FOCA /nit/nit_Input $output_dir/FOCA/$InputFile $BrainMask_dir $FCDandFOCA_TR
+
 
